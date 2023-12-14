eeaf1f5982faeea90890fddc0be9c986fa3ec9a0
Keiran <keinn2210@gmail.com>
2023-12-15 00:06:40 +0800

    went through solutions

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,7 +2,7 @@ import { useRef, forwardRef, useImperativeHandle } from 'react'
 import { createPortal } from 'react-dom'
 
 
-const Checkout = forwardRef( function Checkout({ cart }, ref) {
+const Checkout = forwardRef(function Checkout({ cart }, ref) {
 
     useImperativeHandle(ref, () => {
         return {
@@ -23,27 +23,56 @@ const Checkout = forwardRef( function Checkout({ cart }, ref) {
         total_price += item.price * item.quantity
     })
 
+    //function initially not included in my code, take note when submitting to backend.
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(cart)
+
+        const fd = new FormData(event.target)
+        console.log(fd)
+        const customerData = Object.fromEntries(fd.entries())
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cart,
+                    customer: customerData
+                }
+            })
+        })
+
+
+    }
+
     return (createPortal(<dialog className="modal" id="checkout" ref={dialog}>
-        <h2>Checkout</h2>
-        <p>Total Amount: ${total_price.toFixed(2)}</p>
-        <div className="control">
+
+
+        <form className="control" method="post" onSubmit={(event)=>handleSubmit(event)}>
+            <h2>Checkout</h2>
+            <p>Total Amount: ${total_price.toFixed(2)}</p>
             <label className="control">Full Name</label>
-            <input className="control"></input>
-            <label className="control" type="email">E-Mail Address</label>
-            <input className="control"></input>
+            <input className="control" name='name'></input>
+            <label className="control">E-Mail Address</label>
+            <input className="control" name="email"></input>
             <label className="control">Street</label>
-            <input className="control"></input>
+            <input className="control" name="street"></input>
             <div className="control-row">
                 <label className="control">Postal Code</label>
-                <input className="control"></input>
+                <input className="control" name="postal-code"></input>
                 <label className="control">City</label>
-                <input className="control"></input>
+                <input className="control" name="city"></input>
             </div>
-        </div>
-        <div className="modal-actions">
-            <button className="text-button" onClick={closeModal}>Close</button>
-            <button className="button">Submit Order</button>
-        </div>
+            <p className="modal-actions">
+                <button className="text-button" type="button" onClick={closeModal}>Close</button>
+                <button className="button">Submit Order</button>
+            </p>
+        </form>
+
+
     </dialog>, document.getElementById('modal'))
 
     )
